5fcc359d30327b2908ecc3e7f81982d5804fe0b8
yishuihancheng <623546489@qq.com>
2016-11-29 15:01:13 +0800

    mybatis initial finished

---
+++ src/main/java/edu/scu/cs/tlzhang/entity/User.java
@@ -0,0 +1,25 @@
+package edu.scu.cs.tlzhang.entity;
+
+/**
+ * Created by tlzhang on 2016/11/28.
+ */
+public class User {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
